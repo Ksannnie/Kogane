02d4d41f04c343adbec5543b9090cfe8df80091d
Kevin Sanchez <klsanchez88888@gmail.com>
2026-06-26 17:56:39 -0400

    Add basic text conversation system

--- main.py
+++ main.py
@@ -16,21 +16,24 @@ show_status()
 while True:
     user_input = input(f"{user_name}: ").strip().lower()
 
-    if user_input in["exit", "quit", "bye", "shutdown"]:
-        kogane_speak(f"Session terminated. Bye Bye!, {user_name}.")
+    if user_input == "":
+        kogane_speak(f"Kevin... you gotta type something.")
+    
+    elif any(word in user_input for word in ["exit", "quit", "bye", "shutdown"]):
+        kogane_speak(f"Session terminated. Bye bye!, {user_name}.")
         break
 
-    elif user_input in ["hello, hi", "kogane", "hey"]:
+    elif any(word in user_input for word in ["hello", "hi", "hey", "kogane"]):
         kogane_speak(f"Heyo! {user_name}, how can I assist you today?")
 
-    elif user_input == "status":
+    elif "status" in user_input:
         show_status()
 
-    elif user_input == "mode":
+    elif "mode" in user_input:
         kogane_speak(f"Current mode is {mode}.")
-    
-    elif user_input == "help":
+
+    elif "help" in user_input:
         kogane_speak("Available commands: hello, status, mode, help, exit, quit, bye, shutdown.")
 
     else:
-        kogane_speak("I'm sorry, I didn't understand that command. Type 'help' for a list of commands.")
+        kogane_speak("I'm sorry. I didn't understand that command. Type 'help' for a list of commands.")
